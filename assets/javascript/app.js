0340dfbc6cb6f7022efda0fc9654b3f08a4a06ae
KungFuni0812 <kainichen407@gmail.com>
2021-08-13 20:27:55 -0400

    Initial Commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+//click the start button to start
+// have a list of questions
+// each question have 4 possible answers, only 1 is correct
+// ask the question from the list of questions
+// provide the list of answers
+// the player is given 30 seconds to answer
+// the player chooses one of the 4 possible answers
+    // if they choose the wrong answer, show the right answer and tell them they are wrong
+    // if they are right, say "you're right"
+    // if they run out of time, say "you didn't answer", show the right answer
+// go to the next question
+// continue doing that until they answer all the questions
+// tell the player how many right, how many wrong, and ask if they want to start the game again
+
+
+// 
